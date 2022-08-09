aa9c5c16ff6fc05fafaf5271d4270128b02d6f90
Jaehwi Kwon <jaehwikwon@Jaehwis-MacBook-Pro.local>
2022-08-09 13:19:42 -0500

    add more divs

--- src/App.vue
+++ src/App.vue
@@ -39,39 +39,41 @@ function removePokemonFromTeam(poke) {
 function debug(log) {
   console.log(log)
 }
-
-
-
 </script>
 
 <template>
   <div>
     <h1>Pokemon</h1>
-    <h2>Search Pokemon:</h2>
-    <form @submit.prevent ="searchForPokemon">
-        <input v-model="searchPokemon" placeholder="Pokemon"/>
-        <button>Search</button>
-    </form>
-    <form v-if="pokemon" @submit.prevent ="addPokemonToTeam">
-      {{ pokemon.id }}
-      {{ pokemon.name }}
-      <button>Add to Team</button>
-    </form>
+    <div>
+      <h2>Search Pokemon:</h2>
+      <form @submit.prevent ="searchForPokemon">
+          <input v-model="searchPokemon" placeholder="Pokemon"/>
+          <button>Search</button>
+      </form>
+      <form v-if="pokemon" @submit.prevent ="addPokemonToTeam">
+        {{ pokemon.id }}
+        {{ pokemon.name }}
+        <button>Add to Team</button>
+      </form>
+    </div>
+    
 
-    <h1>My team:</h1>
-    <ul>
-      <li v-for="poke in team" :key="poke.id">
-        {{ poke.name }}
-        {{ debug(poke) }}
-        <ul>
-          <li v-for="type in poke.types">
-            {{ type.type.name }}
-            {{ debug(type) }}
-          </li>
-        </ul>
-        <button @click="removePokemonFromTeam(poke)">X</button>
-      </li>
-    </ul>
+    <div>
+      <h1>My team:</h1>
+      <ul>
+        <li v-for="poke in team" :key="poke.id">
+          {{ poke.name }}
+          {{ debug(poke) }}
+          <ul>
+            <li v-for="type in poke.types">
+              {{ type.type.name }}
+              {{ debug(type) }}
+            </li>
+          </ul>
+          <button @click="removePokemonFromTeam(poke)">X</button>
+        </li>
+      </ul>
+    </div>
 
   </div>
 </template>
